2257bc26a3bfc9a0e3d123238802902fbc2eea9b
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2022-02-25 17:51:23 +0300

    Dummy data for database

---
+++ addressbook.database.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-unused-vars */
+const database = [
+  {
+    id: 1,
+    firstname: 'Shandrak',
+    lastname: 'Ushindi',
+    phonenumber: '256702481032',
+
+  },
+  {
+    id: 2,
+    firstname: 'Gryc',
+    lastname: 'Ushindi',
+    phonenumber: '256702481032',
+
+  },
+  {
+    id: 3,
+    firstname: 'Krystl',
+    lastname: 'Ushindi',
+    phonenumber: '256774869821',
+
+  },
+  {
+    id: 4,
+    firstname: 'Brylon',
+    lastname: 'Ushindi',
+    phonenumber: '256702441425',
+
+  },
+  {
+    id: 5,
+    firstname: 'Kydn',
+    lastname: 'Ushindi',
+    phonenumber: '256770481032',
+
+  },
+  {
+    id: 6,
+    firstname: 'Haddish',
+    lastname: 'Ushindi',
+    phonenumber: '256775481539',
+
+  },
+  {
+    id: 7,
+    firstname: 'Deshya',
+    lastname: 'Ushindi',
+    phonenumber: '0772431809',
+  },
+  {
+    id: 8,
+    firstname: 'Jules',
+    lastname: 'Ushindi',
+    phonenumber: '0782471899',
+  },
+  {
+    id: 9,
+    firstname: 'Shiv',
+    lastname: 'Ushindi',
+    phonenumber: '0777434809',
+  },
+  {
+    id: 10,
+    firstname: 'Dylan',
+    lastname: 'Ushindi',
+    phonenumber: '0777831909',
+  },
+
+];
+module.exports = database;
