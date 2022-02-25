85cbfeb64909c41b92e057a959986523800cd2d7
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2022-02-25 19:06:39 +0300

    Database connection added

--- app.js
+++ app.js
@@ -2,8 +2,30 @@
 /* eslint-disable no-console */
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const app = express();
+
 const port = process.env.PORT || 5000;
+
+// Requiring the dotenv
+require('dotenv').config();
+
+// Creating the mongoose connection.
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useCreateIndex: true,
+});
+
+mongoose.connection
+  .on('open', () => {
+    console.log('Mongoose connection currently open');
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
 
 // Middleware.
 app.use(express.json());
